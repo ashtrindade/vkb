39a5fa62f883a6e493fd453fdace69540e154fca
Ash Trindade <ashtrindade5@gmail.com>
2025-02-08 19:30:09 -0300

    update infos

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ const toggleNav = () => {
   } else {
     // When menu is opened after transition change text respectively
     setTimeout(() => {
-      btnToggleNav.textContent = "close";
+      btnToggleNav.textContent = "fechar";
     }, 475);
   }
 };
@@ -89,26 +89,6 @@ document.body.addEventListener("keydown", (e) => {
     e.preventDefault();
     btnToggleNav.focus();
   }
-});
-
-// Rotating logos animation
-
-const logosWrappers = document.querySelectorAll(".logo-group");
-
-const sleep = (number) => new Promise((res) => setTimeout(res, number));
-
-logosWrappers.forEach(async (logoWrapper, i) => {
-  const logos = Array.from(logoWrapper.children);
-  await sleep(1400 * i);
-  setInterval(() => {
-    let temp = logos[0];
-    logos[0] = logos[1];
-    logos[1] = logos[2];
-    logos[2] = temp;
-    logos[0].classList.add("hide", "to-top");
-    logos[1].classList.remove("hide", "to-top", "to-bottom");
-    logos[2].classList.add("hide", "to-bottom");
-  }, 5600);
 });
 
 yearEl.textContent = new Date().getFullYear();
